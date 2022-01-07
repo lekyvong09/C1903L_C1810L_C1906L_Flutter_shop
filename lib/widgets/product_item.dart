1c6e3cf7e58b7621d2d406cade6a8145774529b8
lekyvong09 <lekyvong09@gmail.com>
2022-01-07 20:10:08 +0700

    navigate product-list to product-detail

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../screens/product_detail_screen.dart';
 
 class ProductItem extends StatelessWidget {
 
@@ -10,15 +11,18 @@ class ProductItem extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(10),
-      child: GridTile(
-        child: Image.network(imageUrl, fit: BoxFit.cover,),
-        footer: GridTileBar(
-          title: Text(name, textAlign: TextAlign.center,),
-          backgroundColor: Colors.black54,
-          leading: IconButton(icon: Icon(Icons.favorite), color: Theme.of(context).accentColor, onPressed: () { },),
-          trailing: IconButton(icon: Icon(Icons.shopping_cart), color: Theme.of(context).accentColor, onPressed: () { },),
+    return GestureDetector(
+      onTap: () => Navigator.of(context).push(MaterialPageRoute(builder: (ctx) => ProductDetailScreen(name))),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(10),
+        child: GridTile(
+          child: Image.network(imageUrl, fit: BoxFit.cover,),
+          footer: GridTileBar(
+            title: Text(name, textAlign: TextAlign.center,),
+            backgroundColor: Colors.black54,
+            leading: IconButton(icon: Icon(Icons.favorite), color: Theme.of(context).accentColor, onPressed: () { },),
+            trailing: IconButton(icon: Icon(Icons.shopping_cart), color: Theme.of(context).accentColor, onPressed: () { },),
+          ),
         ),
       ),
     );
