a93084c9ecd499956ada567e2cf9c366134bc848
lekyvong09 <lekyvong09@gmail.com>
2022-02-07 21:14:29 +0700

    Product CRUD - add product 2

--- lib/screens/product_edit_screen.dart
+++ lib/screens/product_edit_screen.dart
@@ -8,16 +8,29 @@ class ProductEditScreen extends StatefulWidget {
 }
 
 class _ProductEditScreen extends State<ProductEditScreen> {
+  final _priceFocusNode = FocusNode();
+  final _descriptionFocusNode = FocusNode();
+  final _imageFocusNode = FocusNode();
+  final _imageUrlController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Edit Product'),),
       body: Padding(
         padding: EdgeInsets.all(16),
-        child: TextFormField(
-          decoration: InputDecoration(labelText: 'Title'),
-          textInputAction: TextInputAction.next,
-        ),
+        child: Form(child: SingleChildScrollView(child: Column(children: [
+          TextFormField(decoration: InputDecoration(labelText: 'Title'), textInputAction: TextInputAction.next,
+            onFieldSubmitted: (ctx) => FocusScope.of(context).requestFocus(_priceFocusNode),),
+          TextFormField(decoration: InputDecoration(labelText: 'Price'), focusNode: _priceFocusNode, textInputAction: TextInputAction.next, keyboardType: TextInputType.numberWithOptions(signed: true, decimal: true),
+            onFieldSubmitted: (ctx) => FocusScope.of(context).requestFocus(_descriptionFocusNode),),
+          TextFormField(decoration: InputDecoration(labelText: 'Description'), focusNode: _descriptionFocusNode, maxLines: 3, textInputAction: TextInputAction.next, keyboardType: TextInputType.multiline,
+            onFieldSubmitted: (ctx) => FocusScope.of(context).requestFocus(_imageFocusNode),),
+          Row(children: [
+            Container(child: Image.network(_imageUrlController.text)),
+            TextFormField(decoration: InputDecoration(labelText: 'Image URL'), focusNode: _imageFocusNode, controller: _imageUrlController, textInputAction: TextInputAction.done, keyboardType: TextInputType.url,),
+          ],), 
+        ],),),),
       ),
     );
   }
