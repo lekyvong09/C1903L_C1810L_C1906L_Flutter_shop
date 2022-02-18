e17340413078985c0979ec91d40b69f9df793157
lekyvong09 <lekyvong09@gmail.com>
2022-02-18 19:44:57 +0700

    Product CRUD - favorite product

--- lib/models/product.dart
+++ lib/models/product.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as httpClient;
+import 'dart:convert';
 
 class Product with ChangeNotifier{
   int id;
@@ -20,8 +22,33 @@ class Product with ChangeNotifier{
     'isFavorite': isFavorite,
   };
 
-  void toggleFavoriteStatus() {
-    isFavorite = !isFavorite;
-    notifyListeners();
+  Future<void> toggleFavoriteStatus() {
+    Uri url = Uri.parse('http://localhost:8080/api/products/$id');
+    Map<String, String> headers = {
+      'Content-type': 'application/json',
+      'Accept': 'application/json',
+    };
+    
+    return httpClient.put(url, headers: headers, body: json.encode({
+      'name': this.name,
+      'description': this.description,
+      'unitPrice': this.unitPrice,
+      'imageUrl': this.imageUrl,
+      'favorite': !this.isFavorite,
+      'unitsInStock': 100,
+      'active': true,
+      'category': {"id": 5, "categoryName": "MobileProduct"}
+    })).then((response) {
+      if (response.statusCode == 200) {
+        isFavorite = !isFavorite;
+        notifyListeners();
+      } else {
+        try {
+          print(json.decode(response.body));  
+        } on FormatException catch (e) {
+          print('Message return is not a valid JSON format');
+        }
+      }
+    }).catchError((error) => throw error);
   }
 }
