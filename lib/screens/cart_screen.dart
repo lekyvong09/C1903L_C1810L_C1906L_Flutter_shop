e63db294e3621645bb23e02fedfa0437e0c43d0c
lekyvong09 <lekyvong09@gmail.com>
2022-02-18 20:35:34 +0700

    Order CRUD - show loading indicate & prevent multi-click on ORDER NOW

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -5,8 +5,14 @@ import '../models/order.dart';
 import '../models/cart.dart';
 import '../widgets/cart_item_widget.dart';
 
-class CartScreen extends StatelessWidget {
+class CartScreen extends StatefulWidget {
   static const routeName = '/cart';
+  @override
+  State<StatefulWidget> createState() => _CartScreen();
+}
+
+class _CartScreen extends State<CartScreen> {
+  bool _isLoading = false;
 
   @override
   Widget build(BuildContext context) {
@@ -26,10 +32,12 @@ class CartScreen extends StatelessWidget {
                 Text('Total', style: TextStyle(fontSize: 20),),
                 Spacer(),
                 Chip(label: Text('\$${cart.totalAmount.toStringAsFixed(2)}', style: TextStyle(color: Theme.of(context).primaryTextTheme.headline6!.color),), backgroundColor: Theme.of(context).primaryColor,),
-                FlatButton(child: Text('ORDER NOW', style: TextStyle(color: Theme.of(context).primaryColor),),
-                    onPressed: () async {
+                FlatButton(child: _isLoading ? CircularProgressIndicator() : Text('ORDER NOW', style: TextStyle(color: Theme.of(context).primaryColor),),
+                    onPressed: (cart.totalAmount <=0 || _isLoading) ? null : () async {
+                      setState(() { _isLoading = true; });
                       await Provider.of<Orders>(context, listen: false).addOrder(cart.items.values.toList(), cart.totalAmount);
                       cart.clear();
+                      setState(() { _isLoading = false; });
                     }
                 ),
               ],
